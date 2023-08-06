c14844c97debde8731b94da117c656ea6b9eaaab
MSI\hosiu <hosiuchuen05@gmail.com>
2023-08-06 01:04:06 -0400

    building job details pages

---
+++ app/job-details/[id].js
@@ -0,0 +1,23 @@
+import { Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from "react-native"
+import { Stack, useRouter, useLocalSearchParams } from "expo-router"
+import { useCallback, useState } from "react"
+
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from "../../components"
+import { COLORS, icons, SIZES } from "../../constants"
+import useFetch from "../../hook/useFetch"
+
+const JobDetails = () => {
+    const params = useLocalSearchParams();
+    const router = useRouter();
+    const { data, isLoading, error, refetch } = useFetch('job-details', {
+        job_id: params.id,
+    })
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen></Stack.Screen>
+        </SafeAreaView>
+    )
+}
+
+export default JobDetails
